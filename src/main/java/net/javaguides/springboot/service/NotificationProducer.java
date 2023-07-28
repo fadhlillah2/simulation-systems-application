94a63aae418f7d0e4e3635a5bb0d0bfde0acb126
FADHLILLAH <73236007+fadhlillah2@users.noreply.github.com>
2023-07-28 13:41:52 +0700

    comment kafka simulation

--- src/main/java/net/javaguides/springboot/service/NotificationProducer.java
+++ src/main/java/net/javaguides/springboot/service/NotificationProducer.java
@@ -1,19 +1,19 @@
-package net.javaguides.springboot.service;
-
-import net.javaguides.springboot.model.Notification;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Service;
-
-@Service
-public class NotificationProducer {
-
-    private static final String TOPIC = "notifications";
-
-    @Autowired
-    private KafkaTemplate<String, Notification> kafkaTemplate;
-
-    public void sendNotification(Notification notification){
-        this.kafkaTemplate.send(TOPIC, notification);
-    }
-}
+//package net.javaguides.springboot.service;
+//
+//import net.javaguides.springboot.model.Notification;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class NotificationProducer {
+//
+//    private static final String TOPIC = "notifications";
+//
+//    @Autowired
+//    private KafkaTemplate<String, Notification> kafkaTemplate;
+//
+//    public void sendNotification(Notification notification){
+//        this.kafkaTemplate.send(TOPIC, notification);
+//    }
+//}
--- src/main/java/net/javaguides/springboot/web/controller/NotificationController.java
+++ src/main/java/net/javaguides/springboot/web/controller/NotificationController.java
@@ -1,55 +1,55 @@
-package net.javaguides.springboot.web.controller;
-
-import net.javaguides.springboot.model.Notification;
-import net.javaguides.springboot.service.NotificationProducer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.concurrent.CopyOnWriteArrayList;
-
-@Controller
-@RequestMapping("/notifications")
-public class NotificationController {
-
-    private final SimpMessagingTemplate template;
-    private final NotificationProducer notificationProducer;
-    private final CopyOnWriteArrayList<Notification> notifications = new CopyOnWriteArrayList<>();
-
-    @Autowired
-    public NotificationController(SimpMessagingTemplate template, NotificationProducer notificationProducer) {
-        this.template = template;
-        this.notificationProducer = notificationProducer;
-    }
-
-    @KafkaListener(topics = "notifications", groupId = "group_id")
-    public void consume(Notification notification) {
-        if (notification == null) {
-            System.err.println("Received null payload");
-            return;
-        }
-        notifications.add(notification);
-        this.template.convertAndSend("/topic/notification", notification);
-    }
-
-
-    @GetMapping
-    public String main(Model model) {
-        model.addAttribute("notifications", notifications);
-        return "notification_receiver";
-    }
-
-    @RequestMapping(value = "/send", method = {RequestMethod.GET, RequestMethod.POST})
-    public String sendNotification(Notification notification, Model model) {
-        model.addAttribute("notification", new Notification());
-
-        notificationProducer.sendNotification(notification);
-
-        this.template.convertAndSend("/topic/notification", notification);
-
-        return "redirect:/notification_sender";
-    }
-}
+//package net.javaguides.springboot.web.controller;
+//
+//import net.javaguides.springboot.model.Notification;
+//import net.javaguides.springboot.service.NotificationProducer;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.messaging.simp.SimpMessagingTemplate;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.concurrent.CopyOnWriteArrayList;
+//
+//@Controller
+//@RequestMapping("/notifications")
+//public class NotificationController {
+//
+//    private final SimpMessagingTemplate template;
+//    private final NotificationProducer notificationProducer;
+//    private final CopyOnWriteArrayList<Notification> notifications = new CopyOnWriteArrayList<>();
+//
+//    @Autowired
+//    public NotificationController(SimpMessagingTemplate template, NotificationProducer notificationProducer) {
+//        this.template = template;
+//        this.notificationProducer = notificationProducer;
+//    }
+//
+//    @KafkaListener(topics = "notifications", groupId = "group_id")
+//    public void consume(Notification notification) {
+//        if (notification == null) {
+//            System.err.println("Received null payload");
+//            return;
+//        }
+//        notifications.add(notification);
+//        this.template.convertAndSend("/topic/notification", notification);
+//    }
+//
+//
+//    @GetMapping
+//    public String main(Model model) {
+//        model.addAttribute("notifications", notifications);
+//        return "notification_receiver";
+//    }
+//
+//    @RequestMapping(value = "/send", method = {RequestMethod.GET, RequestMethod.POST})
+//    public String sendNotification(Notification notification, Model model) {
+//        model.addAttribute("notification", new Notification());
+//
+//        notificationProducer.sendNotification(notification);
+//
+//        this.template.convertAndSend("/topic/notification", notification);
+//
+//        return "redirect:/notification_sender";
+//    }
+//}
